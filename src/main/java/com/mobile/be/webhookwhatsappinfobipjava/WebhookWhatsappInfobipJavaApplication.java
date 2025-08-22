62dcb44e71e110ec36b7e0eac06ff134788581a7
trung381 <130747543+Trung381@users.noreply.github.com>
2025-08-22 09:35:27 +0700

    first commit

---
+++ src/main/java/com/mobile/be/webhookwhatsappinfobipjava/WebhookWhatsappInfobipJavaApplication.java
@@ -0,0 +1,13 @@
+package com.mobile.be.webhookwhatsappinfobipjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebhookWhatsappInfobipJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebhookWhatsappInfobipJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mobile/be/webhookwhatsappinfobipjava/WebhookWhatsappInfobipJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobile.be.webhookwhatsappinfobipjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebhookWhatsappInfobipJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
